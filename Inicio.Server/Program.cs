416c778f0f63158228750e2699ac1e62b11a2b53
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-09-04 17:58:29 -0300

    Inicio en el desarrollo del Front-End. Nuevo proyecto Client agregado a la solucion

--- Inicio.Server/Program.cs
+++ Inicio.Server/Program.cs
@@ -5,16 +5,15 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-
-
-
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddControllersWithViews().AddJsonOptions(
+builder.Services.AddControllers().AddJsonOptions(
     x=> x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -27,6 +26,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
 
 //---------------------------------------------------------------------------
+
 //agregar el automapper
 
 builder.Services.AddAutoMapper(typeof(Program));
@@ -53,9 +53,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
