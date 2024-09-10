b30d3e5aa8b4e0244e7ed0784b3786767d9acbce
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-09-10 03:06:00 -0300

    Servicio de Post (TDocumento) conectado al Front-End

--- Inicio.Client/Servicios/IHttpServicio.cs
+++ Inicio.Client/Servicios/IHttpServicio.cs
@@ -4,5 +4,6 @@ namespace Inicio.Client.Servicios
     public interface IHttpServicio
     {
         Task<HttpRespuesta<T>> Get<T>(string url);
+        Task<HttpRespuesta<object>> Post<T>(string url, T entidad);
     }
 }
