9aa3d4a436c05595cb42db8e2f7741d2d946b59a
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-09-10 11:46:07 -0300

    Nuevo boton Editar (TDocumentos)

--- Inicio.Client/Program.cs
+++ Inicio.Client/Program.cs
@@ -10,4 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IHttpServicio, HttpServicio>();
 
+
 await builder.Build().RunAsync();
--- Inicio.Client/Servicios/IHttpServicio.cs
+++ Inicio.Client/Servicios/IHttpServicio.cs
@@ -5,5 +5,6 @@ namespace Inicio.Client.Servicios
     {
         Task<HttpRespuesta<T>> Get<T>(string url);
         Task<HttpRespuesta<object>> Post<T>(string url, T entidad);
+        Task<HttpRespuesta<object>> Put<T>(string url, T entidad);
     }
 }
