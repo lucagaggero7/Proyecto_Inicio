1f013732eaef9e356d0d44c1e68e890c5fd4d31b
Stefano Gaggero <stefanolucagaggero@gmail.com>
2024-09-12 01:35:14 -0300

    Notificacion SweetAlert en funcion Eliminar, boton con spinner en Eliminar y Crear.

--- Inicio.Client/wwwroot/js/script.js
+++ Inicio.Client/wwwroot/js/script.js
@@ -1,8 +1,17 @@
-﻿function mostrarAlerta(mensaje) {
+﻿
+
+function mostrarAlerta(mensaje) {
     Swal.fire({
         title: 'Éxito',
         text: mensaje,
         icon: 'success',
         confirmButtonText: 'Aceptar'
     });
+}
+function mostrarSpinner() {
+    document.getElementById('spinner').style.display = 'block';
+}
+
+function ocultarSpinner() {
+    document.getElementById('spinner').style.display = 'none';
 }
